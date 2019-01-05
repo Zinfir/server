1045c100fd688d64e6018207e143cc95d0668783
Zinfir <zinfirmail@gmail.com>
2019-01-05 22:02:48 +0300

    edit sub-menu in product_list app

--- product_list/urls.py
+++ product_list/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('history/', views.product_list, name='history'),
     path('showroom/', views.product_list, name='showroom'),
+    path('all/', views.product_list, name='all'),
+    path('home/', views.product_list, name='home'),
+    path('office/', views.product_list, name='office'),
+    path('modern/', views.product_list, name='modern'),
+    path('furniture/', views.product_list, name='furniture'),
+    path('classic/', views.product_list, name='classic'),
 ]
--- product_list/views.py
+++ product_list/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def product_list(request):
 
-    links_main_menu = {
+    links_menu = {
         'links' : [
             { 'href' : 'main:main' , 'name' : 'main' },
             { 'href' : 'product_list:product_list' , 'name' : 'product_list' },
@@ -10,6 +10,18 @@ def product_list(request):
             { 'href' : 'product_list:showroom' , 'name' : 'showroom' },
             { 'href' : 'contacts:contacts' , 'name' : 'contacts' },
         ],
+        'sub_links' : [
+            { 'href' : 'product_list:all' , 'name' : 'all' },
+            { 'href' : 'product_list:home' , 'name' : 'home' },
+            { 'href' : 'product_list:office' , 'name' : 'office' },
+            { 'href' : 'product_list:modern' , 'name' : 'modern' },
+            { 'href' : 'product_list:furniture' , 'name' : 'furniture' },
+            { 'href' : 'product_list:classic' , 'name' : 'classic' },
+        ],
+        'content' : [
+            'fishnet chair',
+            'another chair',
+        ]
     }
 
-    return render(request, "product_list/product_list.html", links_main_menu)
+    return render(request, "product_list/product_list.html", links_menu)
