efd28493802ba9f7b46714abcc55baf81f5e00a6
Zinfir <zinfirmail@gmail.com>
2019-01-07 23:23:24 +0300

    added classes Product and Product_Category in models.py for product_detail and product_list apps

--- product_list/models.py
+++ product_list/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product_Category(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(
+        max_length=250,
+        blank=True,
+        null=True
+        )
+
+    def __str__(self):
+        return self.name
